6229ba1c6f472475a11fcaf5525ba18b70d0b836
anthonysSlab <crafter.antek@gmail.com>
2023-06-03 20:15:19 +0200

    cleaned up code, made er case insensitive

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use std::process::{Command, exit};
 use std::fs;
 
-fn main() {
-    let mut args: Vec<String> = std::env::args().collect();
-    args.remove(0);
-
-    if args.len() == 0 || args[0] == "-h" || args[0] == "--help" {
-        println!(
+static HELP_MESSAGE: &str =
 "bak - Simple File Backup
 Usage: bak <OPTION> [PATHTOFILE]...
 
@@ -20,57 +15,56 @@ Options:
   -u             Removes the '.bak' extension from all provided files
   -ur            '-u' but removes the original files
   -ua            '-u' but for all '.bak' files within the Working Directory
-  -uar           '-ua' but also removes the original files");
-    } else if args[0] == "-v" || args[0] == "--version" {
-        println!("bak 0.2.4");
-    } else if args[0] == "-b" {
-        args.remove(0);
-        back("cp", args);
-    } else if args[0] == "-br" {
-        args.remove(0);
-        back("mv", args);
-    } else if args[0] == "-ba" {
-        back("cp", detect(false));
-    } else if args[0] == "-bar" {
-        back("mv", detect(false));
-    } else if args[0] == "-u" {
-        args.remove(0);
-        un_back("cp", args);
-    } else if args[0] == "-ur" {
-        args.remove(0);
-        un_back("mv", args);
-    } else if args[0] == "-ua" {
-        un_back("cp", detect(true));
-    } else if args[0] == "-uar" {
-        un_back("mv", detect(true));
-    } else {
-        eprintln!("Invalid Option!\nTry 'bak --help' for more Information. ");
-        exit(2);
-    }
+  -uar           '-ua' but also removes the original files";
 
+static VERSION: &str = "bak 0.3.0";
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    if args.len() == 0 {
+        eprintln!("{}", HELP_MESSAGE);
+        exit(0)
+    }
+    
+    match args[0].to_lowercase().as_str() {
+        "-h" | "--help" => eprintln!("{}", HELP_MESSAGE),
+        "-v" | "--version" => eprintln!("{}", VERSION),
+        "-b" => back("cp", args),
+        "-br" => back("mv", args),
+        "-ba" => back("cp", detect()),
+        "-bar" | "-bra" => back("mv", detect()),
+        "-u" => un_back("cp", args),
+        "-ur" => un_back("mv", args),
+        "-ua" => un_back("cp", detect_bak()),
+        "-uar" | "-ura" => un_back("mv", detect_bak()),
+         _ => eprintln!("Invalid Option!\nTry 'bak --help' for more Information."),
+    }
     exit(0);
 }
 
-fn detect(und: bool) -> Vec<String> {
+fn detect() -> Vec<String> {
     let mut args = vec![];
-    if und {
-        fs::read_dir(".")
-            .expect("Could Not Read Directory!")
-            .filter_map(Result::ok)
-            .filter_map(|path| path.file_name().into_string().ok().filter(|name| name.ends_with(".bak")))
-            .for_each(|file| args.push(file));
-    } else {
-        fs::read_dir(".")
-            .expect("Could Not Read Directory!")
-            .filter_map(Result::ok)
-            .filter_map(|path| path.file_name().into_string().ok())
-            .for_each(|file| args.push(file));
-    }
+    fs::read_dir(".")
+        .expect("Could Not Read Directory!")
+        .filter_map(Result::ok)
+        .filter_map(|path| path.file_name().into_string().ok())
+        .for_each(|file| args.push(file));
+    args
+}
+
+fn detect_bak() -> Vec<String> {
+    let mut args = vec![];
+    fs::read_dir(".")
+        .expect("Could Not Read Directory!")
+        .filter_map(Result::ok)
+        .filter_map(|path| path.file_name().into_string().ok().filter(|name| name.ends_with(".bak")))
+        .for_each(|file| args.push(file));
     args
 }
 
 fn un_back(cmd: &str, args: Vec<String>) {
-    for arg in args {
+    for arg in args.iter().skip(1) {
         let out = Command::new(cmd)
             .arg("-v")
             .arg(arg.clone())
@@ -84,7 +78,7 @@ fn un_back(cmd: &str, args: Vec<String>) {
 }
 
 fn back(cmd: &str, args: Vec<String>) {
-    for arg in args {
+    for arg in args.iter().skip(1) {
         let out = Command::new(cmd)
             .arg("-v")
             .arg(arg.clone())
